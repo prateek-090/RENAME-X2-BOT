30effc1ceab00f6656d6301b1be66680c904cc86
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-04-17 08:47:09 +0000

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -9,44 +9,44 @@ class Scripted(object):
 
 
     HELP_TEXT = """
-<i>Watch Video How to Use Me <a href='https://youtu.be/HnXdu74o34E'>[ ᴄʟɪᴄᴋ ʜᴇʀᴇ ]</a></i>\n
-<i>Send a photo to make it as thumbnail (optional)</i>\n
-<i>Send me any file (or) Media from telegram</i>\n
-<i>Convert files into video use /convert command</i>\n
-<i>Reply to that file with /rename new name.ext</i>\n
-<i>View your thumbnail do /sthumbnail command</i>\n
-<i>Delete your thumbnail do /dthumbnail command</i>"""
+<b>Watch Video How to Use Me <a href='https://youtu.be/HnXdu74o34E'>[ ᴄʟɪᴄᴋ ʜᴇʀᴇ ]</a></b>\n
+<b>Send a photo to make it as thumbnail (optional)</b>\n
+<b>Send me any file (or) Media from telegram</b>\n
+<b>Convert files into video use /convert command</b>\n
+<b>Reply to that file with /rename new name.ext</b>\n
+<b>View your thumbnail do /sthumbnail command</b>\n
+<b>Delete your thumbnail do /dthumbnail command</b>"""
 
 
     ABOUT_TEXT = """
-<b>My Name : <a href='https://t.me/Rename_x2_Bot'>Rename X2 Bot</a></b>\n
-<b>Channel : <a href='https://t.me/space_x_bots'>Space X Bots</a></b>\n
-<b>Version : <a href='https://t.me/Rename_x2_Bot'>0.9.2 beta</a></b>\n
-<b>Source : <a href='https://github.com/Clinton-Abraham'>Click Here</a></b>\n
+<b>My Name : <a href='https://t.me/RenameMLBot'>RenameMLBot</a></b>\n
+<b>Channel : <a href='https://t.me/ML_BotUpdates'>• мℓ вσт υρ∂αтєѕ •</a></b>\n
+<b>Version : <a href='https://t.me/RenameMLBot'>0.9.2 beta</a></b>\n
+<b>Source : <a href='https://github.com/kristy-offl'>Click Here</a></b>\n
 <b>Server : <a href='https://heroku.com'>Heroku</a></b>\n
 <b>Library : <a href='https://github.com/pyrogram'>Pyrogram 1.2.8</a></b>\n
 <b>Language : <a href='https://www.python.org'>Python 3.9.4</a></b>\n
-<b>Developer : <a href='https://t.me/clinton_abraham'>℃ℓ𝖏ήtø𝐧 ᗩ𝔟ⓡ𝐚ꫝαＭ</a></b>\n
-<b>Powered By : <a href='https://t.me/tamilweb57'>Tᴀᴍɪʟᴡᴇʙ Tɢ Nᴇᴛᴡᴏʀᴋ</a></b>\n"""
+<b>Developer : <a href='https://t.me/Itz_Me_Malayaali'>✯°• Kʀɪsᴛʏ Oꜰꜰᴄɪᴀʟ •°✯</a></b>\n
+<b>Powered By : <a href='https://t.me/ML_BotUpdates'>• мℓ вσт υρ∂αтєѕ •</a></b>\n"""
 
 
-    CUSTOM_CAPTION = "<i>{}</i>"
-    PROCESSING_TEXT = "<i>Processing...</i>"
+    CUSTOM_CAPTION = "<b>{}</b>"
+    PROCESSING_TEXT = "<b>Processing...</b>"
     ACCESS_DENIED = "<b>¥ou Are Banned 🚫</b>"
-    BANNED_USER_TEXT = "<i>¥ou Are Banned 🚫</i>"
-    TRYING_TO_UPLOAD = "<i>Trying to upload....</i>"
-    CURRENT_THUMBNAIL = "<i>Your Current Thumbnail 🎭</i>"
-    THUMBNAIL_SAVED = "<i>Your Thumbnail Saved ✅</i>"
-    THUMBNAIL_DELETED = "<i>Your Thumbnail Deleted ✅</i>"
-    NO_THUMBNAIL_FOUND = "<i>No Thumbnail Found 😟</i>"
-    TRYING_TO_DOWNLOAD = "<i>Trying To Download....</i>"
-    UPLOAD_SUCCESS = "<u><i>Thank u for using me 🥰</i></u>"
-    REPLY_TO_VIDEO = "<i>Reply to that file with /convert</i>"
-    UPLOAD_START = "<i>Uploading your file please wait...</i>\n"
-    DOWNLOAD_START = "<i>Downloding your file please wait...</i>\n"
-    JOIN_NOW_TEXT = "<code>Join My Update Channel To Use Me</code>"
-    REPLY_TO_FILE = "<i>Reply to that file with /rename new name.ext</i>"
-    CONTACT_MY_DEVELOPER = "<i>Something Wrong Contact My Developer 🤯</i>"
-    REPLY_TO_MEDIA = "<i>Reply to that Media with /rename_video New Name</i>"
-    START_TEXT = "<i>Simple file rename bot with permanent thumbnail support 💯</i>"
-    UPGRADE_TEXT = "<b>To upgrade your subscription <a href='https://t.me/Anydl_x_bot'>[ Click Here ]</a></b>"
+    BANNED_USER_TEXT = "<b>¥ou Are Banned 🚫</b>"
+    TRYING_TO_UPLOAD = "<b>Trying to upload....</b>"
+    CURRENT_THUMBNAIL = "<b>Your Current Thumbnail 🎭</b>"
+    THUMBNAIL_SAVED = "<b>Your Thumbnail Saved ✅</b>"
+    THUMBNAIL_DELETED = "<b>Your Thumbnail Deleted ✅</b>"
+    NO_THUMBNAIL_FOUND = "<b>No Thumbnail Found 😟</b>"
+    TRYING_TO_DOWNLOAD = "<b>Trying To Download....</b>"
+    UPLOAD_SUCCESS = "<u><b>Thank u for using me Share & Support @ML_BotUpdates 🥰</b></u>"
+    REPLY_TO_VIDEO = "<b>Reply to that file with /convert</b>"
+    UPLOAD_START = "<b>Uploading your file please wait...</b>\n"
+    DOWNLOAD_START = "<b>Downloding your file please wait...</b>\n"
+    JOIN_NOW_TEXT = "<code>Hey Dude Join My Update Channel To Use Me</code>"
+    REPLY_TO_FILE = "<b>Reply to that file with /rename new name.ext</b>"
+    CONTACT_MY_DEVELOPER = "<b>Something Wrong Contact My Developer 🤯</b>"
+    REPLY_TO_MEDIA = "<b>Reply to that Media with /rename_video New Name</b>"
+    START_TEXT = "<b>Simple file rename bot with permanent thumbnail support 💯</b>"
+    UPGRADE_TEXT = "<b>To upgrade your subscription <a href='https://t.me/Itz_Me_Malayaali'>[ Click Here ]</a></b>"
