c62d3414ab96038941f425defd8e74405d1f68da
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-05-04 18:54:51 +0530

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -51,5 +51,5 @@ class Scripted(object):
     REPLY_TO_FILE = "<b>Reply to that file with /rename new name.ext</b>"
     CONTACT_MY_DEVELOPER = "<b>Something Wrong Contact Our Support Group @ML_SupportGroup 🤯</b>"
     REPLY_TO_MEDIA = "<b>Reply to that Media with /rename_video New Name</b>"
-    START_TEXT = "<b>Hy..{}</b>\n\n<b>I'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail support!💯</b>"
+    START_TEXT = "<b>Hy..{}</b>\n\n<b>I'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail support!💯</b>\n\n<b>🌀PowerdeD By : <a href='https://t.me/ML_BotUpdates'>• мℓ вσт υρ∂αтєѕ •</a></b>\n\n<b>🌈Developer : <a href='https://t.me/Itz_Me_Malayaali'>✯°• Kʀɪsᴛʏ Oꜰꜰᴄɪᴀʟ •°✯<"
     UPGRADE_TEXT = "<b>To upgrade your subscription <a href='https://t.me/Itz_Me_Malayaali'>[ Click Here ]</a></b>"
